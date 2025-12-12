ba4d9d7d2b14889d5922082beaec08a109b67d86
Edwin8852 <edwinraj2458i@gmail.com>
2025-12-12 13:22:46 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import Login from "./login.js";
+
+function App() {
+  return <Login />;
+}
+
+export default App;
---
+++ src/login.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [msg, setMsg] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    const res = await axios.post("http://localhost:5000/api/auth/login", {
+      email,
+      password
+    });
+
+    setMsg(res.data.message);
+
+    if (res.data.token) {
+      localStorage.setItem("token", res.data.token);
+    }
+  };
+
+  return (
+    <div style={{ maxWidth: "350px", margin: "100px auto" }}>
+      <h2>Login</h2>
+
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="Enter Email"
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        /><br /><br />
+
+        <input
+          type="password"
+          placeholder="Enter Password"
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        /><br /><br />
+
+        <button type="submit">Login</button>
+      </form>
+
+      <p>{msg}</p>
+    </div>
+  );
+}
+
+export default Login;
